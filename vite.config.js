42f136d98897c7f48e29052ad68e032a3c5bdae9
msm691 <marley.sedlak-martin@epitech.eu>
2025-11-27 23:58:54 +0100

    Change base path in Vite config to root

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ export default defineConfig({
   plugins: [react()],
   // Remplacer 'NOM_DU_REPO' par le nom exact de votre dépôt GitHub entre les slashs
   // Exemple: base: '/site-akuno/',
-  base: '/akunowebsite/', 
+  base: '/', 
 })
